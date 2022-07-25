9544320dce81a00141eae9b15091ff10b4c38cb9
Hristo Parteniev <hristo.parteniev@adastragrp.com>
2022-07-25 10:50:02 +0300

    v.2

--- task125_temperatures.py
+++ task125_temperatures.py
@@ -1,3 +1,13 @@
+""" Function checking value"""
+def is_number(string):
+    """ Checking if number is value is valid temperature"""
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+
 """prompting user for absolute file path"""
 file_location = input("Please insert 'temperatures.txt' file path.")
 
@@ -13,9 +23,14 @@ try:
 except FileNotFoundError as e:
     print("Wrong or not existing file/filepath.")
 
+
 for x in source_values:
-    x = x.strip()
-    if x != "":
+    x = x.strip("\n")
+    for char in x:
+        if char == " ":
+            x = x.replace(" ","")
+    y = x[:-1]
+    if x != "" and is_number(y) is True:
         if x[-1] == "F":
             convert_values.append(x)
         elif x[-1] == "C":
@@ -24,12 +39,21 @@ for x in source_values:
             f_conv_val = round(float(value) * 9/5 + 32, 2)
             f_string = f"{f_conv_val}F"
             convert_values.append(f_string)
+    else:
+        print(f"'{x}' is not in the correct format and has been omitted.")
+        continue
 
 with open('F_temperatures.txt', 'w', encoding="utf-8") as final_file:
     for x in convert_values:
         final_file.write(f"{x}\n")
 
 """Input values used:
+12$C
+aaaaaasd123F
+FFASDa$$#@#C
+    a     s        d  C
+a s  d  F
+                 
 10.55 F
 11.1F
 
