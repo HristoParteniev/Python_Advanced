75b351efb37effc72b0cfe8aa85998e9401a58df
HristoParteniev <109510307+HristoParteniev@users.noreply.github.com>
2024-08-29 10:38:21 +0300

    Update task1_generators.py
    add '__main__'

--- task1_generators.py
+++ task1_generators.py
@@ -1,3 +1,4 @@
+"""imports"""
 import os
 import random
 from datetime import datetime, timedelta
@@ -7,11 +8,11 @@ def consecutive_numbers_generator():
     for i in range(1, 21):
         yield i
 
-def generate_random_word(n):
+def generate_random_word(number):
     """ Generating words """
     while True:
         result = ''
-        for _ in range(n):
+        for _ in range(number):
             result += chr(random.randint(97,122))    # generate letters a-z
         yield result
 
@@ -27,7 +28,7 @@ def generate_random_timestamp():
 def generate_random_bool():
     """ Generating boolean values """
     while True:
-       yield random.choice([True,False])
+        yield random.choice([True,False])
 
 if __name__ == '__main__':
     num_gen = consecutive_numbers_generator()
@@ -35,8 +36,12 @@ if __name__ == '__main__':
     ts_gen = generate_random_timestamp()
     bool_gen = generate_random_bool()
 
-    desktop_path = os.path.join(os.path.expanduser("~"), 'OneDrive - Adastra, s.r.o\\Desktop\\Task1_Generators_python_by_Hristo_Parteniev.txt')
+    desktop_path = os.path.join(os.path.expanduser("~"),
+    'OneDrive - Adastra, s.r.o\\Desktop\\Task1_Generators_python_by_Hristo_Parteniev.txt')
 
     with open(desktop_path, 'w', encoding='utf-8') as file:
         for _ in range(20):
-            file.write(f'"{next(num_gen)}","{next(word_gen)}","{next(ts_gen)}","{next(bool_gen)}"'+ '\n')
+            file.write(f'"{next(num_gen)}",\
+                "{next(word_gen)}",\
+                    "{next(ts_gen)}",\
+                        "{next(bool_gen)}"'+ '\n')
